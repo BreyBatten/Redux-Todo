81b810a331951bef9b34195477c4fa70ef66da08
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-06-21 22:39:40 -0400

    built out AddTodo.js and added functionality

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 
 function App() {
   return (
     <div className="App">
       <h1>Todo Redux App</h1>
       <TodoList />
+      <AddTodo />
     </div>
   );
 }
---
+++ todo/src/components/AddTodo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addNewTodo } from '../actions';
+
+class AddTodo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTodo: ''
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addNewTodo(this.state.newTodo);
+        this.setState({ newTodo: ''})
+    }
+
+    handleChange = e => {
+        this.setState({ newTodo: e.target.value })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type= 'text'
+                    placeholder= 'Add A New To-Do!'
+                    value= {this.state.newTodo}
+                    onChange={this.handleChange}
+                />
+                <button onClick={this.handleSubmit}>Submit</button>
+            </form>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        todos: state.todos
+    }
+}
+
+export default connect(mapStateToProps, { addNewTodo })(AddTodo);
