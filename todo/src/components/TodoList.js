77a2d3f4c9a4888ea423f435002faac4745a19f5
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-06-21 22:03:06 -0400

    built out TodoList component with toggle functionality

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { addNewTodo, toggleTodo } from '../actions';
+import { toggleTodo } from '../actions';
 
 class TodoList extends React.Component {
-    state = {
-        newTodo: ""
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTodo: ""
+        };
+    }
+
+    handleToggle = (e, index) => {
+        e.preventDefault();
+        this.props.toggleTodo(index);
+    }
+    
 
     render() {
         return (
             <div className="todo-list">
                 
+                <h2>My To-Do List</h2>
+                <ul>
+                    {this.props.todos.map((item, index) =>
+                    <li key={index} onClick={e => this.handleToggle(e, index)} >
+                        {item.value} {item.completed && <strong>completed!</strong>}
+                    </li>)}
+                </ul>
             </div>
         )
     }
 }
 
-export default TodoList;
+const mapStateToProps = state => {
+    return {
+        todos: state.todos
+    }
+}
+
+export default connect(mapStateToProps, { toggleTodo })(TodoList);
